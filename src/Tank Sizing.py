7fd698b91002e940bb2266e2d12e1132f4a606d3
Angel6871 <91727236+Angel6871@users.noreply.github.com>
2025-05-14 10:00:54 +0200

    Tank Sizing 2

--- src/Tank Sizing.py
+++ src/Tank Sizing.py
@@ -8,14 +8,35 @@ materials_properties = {
     "Annealed Ti-6Al-4V": {"density": 4400, "strength": 910},
     "Inconel 718": {"density": 8300, "strength": 1190}
 }
-
-propellant_mass = 0
-propellant_density = 0
-propellant_pressure = 0
+#Constraints
 tank_diameter = 7
+LH2_LOX_O_F_ratio = 6/1
+LH2_volume_shrinkage_coefficient = 1.2 #%
+LOX_volume_shrinkage_coefficient = 1.43 #%
+CH4_volume_shrinkage_coefficient = 1.28 #%
+
+LH2_pressure = 1035 #kPa (10 bar)
+LH2_density = 0.07085 * 10^3 #kg/m3
+LOX_pressure = 2330,47 #kPa (23 bar)
+Ch4_pressure = 0
+
+#HydroLOx Jarvis
+propellant_mass_jarvis = 14.7 *10^3 #14.7 tons
+structural_mass_jarvis = 2.03 *10^3 #2.03 tons
+oxydizer_mass_jarvis = 6*propellant_mass_jarvis
+
+#HydroLox Spaceshuttle
+propellant_mass_spaceshuttle = 19.1 * 10^3
+structural_mass_spaceshuttle = 2.7 * 10^3
+
+#MethaLox Starship
+propellant_mass_starship = 18.61 *10^3 #18.61 tons
+structural_mass_starship = 1.37 * 10^3 #1.37 tons
+
 
 def tank_volume():
     ##
+    return volume
 # calculating tank thickness
 def calculate_tank_thickness(tank_diameter, tank_length, propellant_pressure, allowable_stress):
     # Assuming a thin-walled cylinder for simplicity
