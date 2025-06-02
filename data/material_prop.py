a7dc1963b303484d69929b9a0fc43f5b40d37da9
dagreat221 <45536905+dagreat221@users.noreply.github.com>
2025-06-02 08:46:27 +0200

    added material propertie for stainless steel.

---
+++ data/material_prop.py
@@ -0,0 +1,5 @@
+"File contains materials properties of various materials"
+
+ss_E = 200*10**9 # Pa, steel
+ss_rho = 7850 # kg/m3, steel
+ss_alpha_expansion = 10*10**(-6) #m/mK
--- src/fatigue_calculation_tool.py
+++ src/fatigue_calculation_tool.py
@@ -7,12 +7,23 @@ current_dir = Path(__file__).parent
 sys.path.append(str(current_dir.parent))
 
 from data import constants as cs
+from data import material_prop as mp
 
 
-print(cs.g_0)
-
+#Functions
 def thermal_stress(delta_T, young_mod, thermal_expansion_coeff):
     return delta_T * young_mod * thermal_expansion_coeff
 
+def pressure_stress(P,r,t,D):
+    hoop_stress = P * r / t
+    head_stress = 0.5*P*2*r/t # FOR 2:1 ELLIPTICAL HEADS
+    return hoop_stress, head_stress
 
+def crack_growth(paris_coeff_C, paris_exp_m, sigma_max, sigma_min, Y, a):
+    delta_K = Y * (sigma_max -sigma_min) * (np.pi*a)
+    da_dn = paris_coeff_C * (delta_K ** paris_exp_m)
+    return da_dn  
 
+sigma_thermal = thermal_stress(273, mp.ss_E, mp.ss_alpha_expansion)
+sigma_launch = 100*10**6
+sigma_pressure_wall, sigma_pressure_head = pressure_stress(10**7, 3.5, 0.01, 1)
