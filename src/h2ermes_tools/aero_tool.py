a2ba1034b147c73f9868870657fcb433021f69f8
s0hamburger <152688820+s0hamburger@users.noreply.github.com>
2025-06-03 10:45:18 +0200

    i did more things - SK

--- src/h2ermes_tools/aero_tool.py
+++ src/h2ermes_tools/aero_tool.py
@@ -2,13 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class bluntBody:
-    def __init__(self, B, c, vel, rho_at, Rc,a):
+    def __init__(self, B, c, vel, rho_at, Rc, a, velocities, rho_atmos, aoa, m):
         self.a = a           # Cone length
         self.B = B           # Cone maximum radius
         self.c = c           # Cone to sphere base (arc height of hemisphere)
         self.vel = vel       # velocity for aerodynamic calculations
         self.rho_at = rho_at # atmospheric density
         self.Rc = Rc         # Cone minimum radius
+        self.vels = velocities
+        self.rho_atmos = rho_atmos
+        self.aoa = aoa #angle of attack during glide
+        self.m = m #mass
 
         #derived geometery
         self.R = (2*B)**2 / (8*c) + c/2          #Radius of the hemisphere
@@ -18,33 +22,66 @@ class bluntBody:
         self.w = np.arctan(B / ((B*a) / (B-Rc)))
 
 
-    def compute_aerodynamics(self):
-        self.alpha = np.linspace(-1 * self.alpha_max, self.alpha_max, 100)
-        self.C_x = 0.5 * (np.sin(self.alpha)**2) * (np.sin(self.mu_b)**2) + \
-              (1 + np.cos(self.mu_b)**2) * (np.cos(self.alpha)**2)
-        self.C_y = np.sin(self.alpha) * np.cos(self.alpha) * (np.sin(self.mu_b)**2)
+    def compute_aerodynamics(self, mode):
+        if mode == 1: #computing aerodynamics for the structure with respect to all angles of attacks
+            self.alpha = np.linspace(-1 * self.alpha_max, self.alpha_max, 100)
+            self.alpha = np.linspace(0, self.alpha_max, 100)
+            
+            self.C_x = 0.5 * (np.sin(self.alpha)**2) * (np.sin(self.mu_b)**2) + \
+                (1 + np.cos(self.mu_b)**2) * (np.cos(self.alpha)**2)
+            self.C_y = np.sin(self.alpha) * np.cos(self.alpha) * (np.sin(self.mu_b)**2)
 
-        self.C_x_tilde = []
-        self.C_y_tilde = []
-        for a in self.alpha:
-            rho_str = np.arccos(-1 * np.tan(self.w) / np.tan(a))
-            if np.abs(a) >= self.w:
-                C_x_t = -1*((np.sin(a)**2)*(np.cos(self.w)**2)/np.pi)*(((1 + (2*(np.cos(rho_str)**2)))*(np.pi - rho_str)) + (3*np.sin(rho_str)*np.cos(rho_str)))
-                self.C_x_tilde.append(C_x_t)
-                C_y_t = (2 * (np.sin(a))**2 * (np.cos(self.w))**2 / np.pi)*(((np.pi - rho_str)*(np.cos(rho_str))) + ((np.sin(rho_str)/3)*(2 + (np.cos(rho_str))**2)))*(1/np.tan(self.w))
-                self.C_y_tilde.append(C_y_t)
-            else:
-                self.C_x_tilde.append(0)
-                self.C_y_tilde.append(0)
-        
-        self.D_x = 0.5 * self.rho_at * self.vel**2 * np.pi*self.B**2 * (self.C_x + self.C_x_tilde)
+            self.C_x_tilde = []
+            self.C_y_tilde = []
+            for a in self.alpha:
+                rho_str = np.arccos(-1 * np.tan(self.w) / np.tan(a))
+                print(a, self.w)
+                if np.abs(a) >= self.w:
+                    C_x_t = -1*((np.sin(a)**2)*(np.cos(self.w)**2)/np.pi)*(((1 + (2*(np.cos(rho_str)**2)))*(np.pi - rho_str)) + (3*np.sin(rho_str)*np.cos(rho_str)))
+                    self.C_x_tilde.append(C_x_t)
+                    C_y_t = (2 * (np.sin(a))**2 * (np.cos(self.w))**2 / np.pi)*(((np.pi - rho_str)*(np.cos(rho_str))) + ((np.sin(rho_str)/3)*(2 + (np.cos(rho_str))**2)))*(1/np.tan(self.w))
+                    self.C_y_tilde.append(C_y_t)
+                else:
+                    self.C_x_tilde.append(0)
+                    self.C_y_tilde.append(0)
+            
+            self.D_x = 0.5 * self.rho_at * self.vel**2 * np.pi*self.B**2 * (self.C_x + self.C_x_tilde)
+            self.L_y = 0.5 * self.rho_at * self.vel**2 * np.pi*self.B**2 * (self.C_y + self.C_y_tilde)
+
+            self.D_x_tot = self.C_x_tilde + self.C_x
+            self.C_y_tot = self.C_y + self.C_y_tilde
+
+            #calculating lift curve slope
+            self.Cl_alpha = (self.C_y_tot[1] - self.C_y_tot[-1]) / (self.alpha[1] - self.alpha[-1])
+            print("Cl_alpha = ", self.Cl_alpha)
+        elif mode == 2: #computing aerodynamics for a specific angle of attack for a range of velocities and densities
+            self.C_x_aoa = 0.5 * (np.sin(self.aoa)**2) * (np.sin(self.mu_b)**2) + \
+                (1 + np.cos(self.mu_b)**2) * (np.cos(self.aoa)**2)
+            self.C_y_aoa = np.sin(self.aoa) * np.cos(self.aoa) * (np.sin(self.mu_b)**2)
+            rho_str = np.arccos(-1 * np.tan(self.w) / np.tan(self.aoa))
+            if self.aoa >= self.w:
+                self.C_x_cone_aoa = -1*((np.sin(self.aoa)**2)*(np.cos(self.w)**2)/np.pi)*(((1 + (2*(np.cos(rho_str)**2)))*(np.pi - rho_str)) + (3*np.sin(rho_str)*np.cos(rho_str)))
+                self.C_y_cone_aoa = (2 * (np.sin(self.aoa))**2 * (np.cos(self.w))**2 / np.pi)*(((np.pi - rho_str)*(np.cos(rho_str))) + ((np.sin(rho_str)/3)*(2 + (np.cos(rho_str))**2)))*(1/np.tan(self.w))
+
+            self.drags = []
+            for i in range(len(self.vels)):
+                self.drags.append(0.5 * self.rho_atmos[i] * self.vels[i]**2 * np.pi*self.B**2 * (self.C_x_aoa + self.C_x_cone_aoa))
+            
+
     
-    def plots(self):
-        plt.plot(self.alpha * 180/np.pi, self.C_x + self.C_x_tilde, label="Cx coefficient", color="blue")
-        plt.plot(self.alpha * 180/np.pi, self.C_y + self.C_y_tilde, label="Cy coefficient", color="red")
-        plt.plot(self.alpha * 180/np.pi, self.D_x, label = "Drag force", color = "black")
-        plt.legend()
-        plt.show()
+    def plots(self, mode):
+        if mode == 1:
+            plt.plot(self.alpha * 180/np.pi, self.C_x + self.C_x_tilde, label="Cx coefficient", color="blue")
+            plt.plot(self.alpha * 180/np.pi, self.C_y + self.C_y_tilde, label="Cy coefficient", color="red")
+            #plt.plot(self.alpha * 180/np.pi, self.D_x, label = "Drag force", color = "black")
+            #plt.plot(self.alpha * 180/np.pi, self.L_y, label = "Lift force", color = "black")
+
+            plt.legend()
+            plt.show()
+        elif mode == 2:
+            plt.plot(self.drags, self.rho_atmos, label="Drag through flight regime", color="red")
+            plt.legend()
+            plt.show()
 
     def draw_geom(self):
         #hemisphere meshgrid
@@ -86,7 +123,7 @@ class bluntBody:
 
         plt.show()
 
-    def stability(self):
+    def moments(self):
         Y = np.array(self.C_y)
         Y_tilde = np.array(self.C_y_tilde)
         self.xb = self.R*(1 - np.cos(self.mu_b))
@@ -96,10 +133,26 @@ class bluntBody:
         self.x_moment_arm = ((self.R*self.Y_cap) + (x_tilde*self.Y_cone)) / (self.Y_cap + self.Y_cone)
         
         plt.xlabel("Moment Arm")
+        plt.xlabel("Moments Nm")
         plt.ylabel("Angle of Attack")
-        plt.plot(self.x_moment_arm, self.alpha * 180/np.pi, label = "moment arm", color = "black")
+        #plt.plot(self.x_moment_arm, self.alpha * 180/np.pi, label = "moment arm", color = "black")
+        plt.plot((self.R*self.Y_cap), self.alpha * 180/np.pi, label = "moment on cap", color = "red")
+        plt.plot((x_tilde*self.Y_cone), self.alpha * 180/np.pi, label = "moment on cone", color = "blue")
         plt.legend()
         plt.show()
+    
+    def dynamic_stability(self):
+        self.I = self.B**2 * self.m * ((3*self.a + 8*self.B)/(10*(self.a + 2*self.B)))
+        print(self.I)
+        self.rg = np.sqrt(self.I / self.m)
+        print(self.rg)
+        self.Cmq_Cmadot = ((4*self.I) / (self.m*self.B**2)) * self.Cl_alpha
+        print(self.Cmq_Cmadot)
+        self.damping = self.D_x_tot - self.Cl_alpha + (2*self.B / self.rg)*self.Cmq_Cmadot
+        plt.plot(self.alpha * 180/np.pi, self.damping, label='damping coefficient')
+        plt.show()
+        #print(self.damping)
+
 
 if __name__ == "__main__":
     B = 4.94
@@ -107,13 +160,19 @@ if __name__ == "__main__":
     vel = 4000
     rho_at = 3.31E-04
     Rc = 2.47
-    a = 17.57
+    a = 10
+    vels = [1000,2000,3000,4000,5000,6000,7000]
+    rho_atmos = [0.1, 0.5, 0.7, 0.8, 0.9, 1.1, 1.5]
+    aoa = 30 * np.pi/180
+    m = 10000
 
-    body = bluntBody(B, c, vel, rho_at, Rc, a
+    body = bluntBody(B, c, vel, rho_at, Rc, a, vels, rho_atmos, aoa, m
     )
 
-    body.compute_aerodynamics()
+    body.compute_aerodynamics(mode = 1)
 
-    body.plots()
+    body.plots(mode = 1)
 
-    body.stability()
+    body.dynamic_stability()
+
+    #body.draw_geom()
