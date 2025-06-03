ab8ae54cf6fd871565d08487e7d5310db2e97e54
lgarciagutierr-git <lgarciagutierr@tudelft.nl>
2025-06-03 10:19:33 +0200

    print statements to get areas at throat and exit of the nozzle

--- Propulsion/CEA-investigation-continued.py
+++ Propulsion/CEA-investigation-continued.py
@@ -1,9 +1,19 @@
 import scipy as sp
 import numpy as np
 from rocketcea.cea_obj_w_units import CEA_Obj
+import rocketcea 
 import matplotlib.pyplot as plt
 
+
+
 C = CEA_Obj( oxName='LOX', fuelName='LH2', pressure_units='Pa', cstar_units='m/s', temperature_units='K', sonic_velocity_units='m/s', enthalpy_units='J/kg', density_units='kg/m^3', specific_heat_units= 'J/kg-K', viscosity_units='millipoise', thermal_cond_units='mcal/cm-K-s')
+
+Pc = 6000000
+MR = 6.0
+eps = 120
+
+Isp = C.get_Isp(Pc, MR, eps=40.0, frozen=0, frozenAtThroat=0)
+thrust = 66700 #in N
 
 def obtain_cstar(Pc, MR):
     """
@@ -20,12 +30,18 @@ def obtain_cstar(Pc, MR):
 
 
 def obtain_exit_pressure(Pc, MR, eps, frozen=0, frozenAtThroat=0): 
-    pressure_ratio = C.get_PcOvPePc(Pc= 6000000, MR=6, eps=, frozen=0, frozenAtThroat=0)
+    pressure_ratio = C.get_PcOvPePc(Pc= 6000000, MR=6, eps=eps, frozen=0, frozenAtThroat=0)
     P_exit = Pc / pressure_ratio
     return P_exit
 
+Pc = 6000000
 
-def area_throat(thrust, Isp, g_0=9.81, Pc):
+def area_throat(thrust, Isp = Isp, g_0=9.81, Pc= Pc):
     mass_flow = thrust / (g_0 * Isp)
     area_throat = (mass_flow * c_star) / Pc
     return area_throat
+
+print("exit presssure is", obtain_exit_pressure(Pc, MR, eps))
+print("exit area is", area_throat(thrust, Isp=Isp, Pc=Pc)*eps)
+print("exit diameter is", (((area_throat(thrust, Isp=Isp, Pc=Pc)*eps))/np.pi)**0.5 * 2)
+print("area of the throat", area_throat(thrust, Isp=Isp, Pc=Pc))
