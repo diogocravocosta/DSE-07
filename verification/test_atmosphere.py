9513f9ebe500e80e5b632188c06c46802fdfa177
andreea <alupei@student.tudelft.nl>
2025-06-04 12:02:55 +0200

    earth radius changeed

--- verification/test_atmosphere.py
+++ verification/test_atmosphere.py
@@ -10,7 +10,7 @@ class TestAtmosphere:
     def make_test_atmosphere(self):
         atmosphere = atm.Atmosphere.__new__(atm.Atmosphere)
         atmosphere.g_0 = ct.g_0  # m/s^2
-        atmosphere.Re = ct.radius  # m
+        atmosphere.Re = ct.earth_radius  # m
         atmosphere.R_star = ct.R_star
         atmosphere.M = ct.molecular_mass
         atmosphere.R = atmosphere.R_star / atmosphere.M
