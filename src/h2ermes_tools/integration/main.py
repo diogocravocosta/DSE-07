405ac1fb97291f705ab02ed6c305282672edee27
Matyas Karasek <m.karasek@student.tudelft.nl>
2025-06-06 11:10:43 +0200

    initial integration code

---
+++ src/h2ermes_tools/integration/main.py
@@ -0,0 +1,16 @@
+from h2ermes_tools.variables import landing_delta_v
+
+
+
+payload_mass = 10_000.0
+sea_level_isp = 360
+vacuum_isp = 450
+
+landing_delta_v = 525
+deorbit_delta_v = 180
+circularization_delta_v = 150
+orbit_raising_delta_v = 150
+orbit_insertion_delta_v = 6000
+
+initial_dry_mass = 30_000.0
+
