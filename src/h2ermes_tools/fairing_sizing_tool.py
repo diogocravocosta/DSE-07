7cc92c3baa682b8ff2fe69c9e07881d5547ce6ee
dagreat221 <45536905+dagreat221@users.noreply.github.com>
2025-06-06 11:12:29 +0200

    made file for fairing

---
+++ src/h2ermes_tools/fairing_sizing_tool.py
@@ -0,0 +1,9 @@
+import numpy as np
+from pathlib import Path
+import sys
+import matplotlib.pyplot as plt
+current_dir = Path(__file__).parent
+sys.path.append(str(current_dir.parent))
+from data import constants as cs
+from data import material as mat
+
