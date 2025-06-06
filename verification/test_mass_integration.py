0a4330711a9ec4103c8cb7fe385aebe97a8efa6e
Matyas Karasek <m.karasek@student.tudelft.nl>
2025-06-06 08:57:51 +0200

    added hydrogen oxygen calculation test

--- verification/test_mass_integration.py
+++ verification/test_mass_integration.py
@@ -39,10 +39,33 @@ def test_calculate_propellant_mass():
     expected_transfer_mass = 3720.8679
     expected_insertion_mass = 163_885.6476
 
+    expected_propellant_mass = 174051.0321
+
     expected_total_mass = 220501.032
 
     assert np.isclose(integrator.landing_propellant_mass, expected_landing_mass, rtol=1e-4)
     assert np.isclose(integrator.deorbit_propellant_mass, expected_deorbit_mass, rtol=1e-4)
     assert np.isclose(integrator.transfer_propellant_mass, expected_transfer_mass, rtol=1e-4)
     assert np.isclose(integrator.orbit_insertion_propellant_mass, expected_insertion_mass, rtol=1e-4)
+    assert np.isclose(integrator.propellant_mass, expected_propellant_mass, rtol=1e-4)
     assert np.isclose(integrator.total_mass, expected_total_mass, rtol=1e-4)
+
+def test_calculate_hydrogen_oxygen_mass():
+    integrator = make_mass_integrator()
+
+    integrator.propellant_mass = 174051.0321
+    integrator.deorbit_propellant_mass = 1634.4715
+    integrator.landing_propellant_mass = 4810.0451
+
+    expected_hydrogen_mass = 29089.43316
+    expected_oxygen_mass = 150411.5989
+
+    expected_header_hydrogen_mass = 920.6452
+    expected_header_oxygen_mass = 5523.8714
+
+    integrator.calculate_hydrogen_oxygen_mass()
+
+    assert np.isclose(integrator.hydrogen_mass, expected_hydrogen_mass, rtol=1e-4)
+    assert np.isclose(integrator.oxygen_mass, expected_oxygen_mass, rtol=1e-4)
+    assert np.isclose(integrator.header_hydrogen_mass, expected_header_hydrogen_mass, rtol=1e-4)
+    assert np.isclose(integrator.header_oxygen_mass, expected_header_oxygen_mass, rtol=1e-4)
