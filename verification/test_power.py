98666dc8be39a76ff3ab178ede206512129b3452
Matyas Karasek <m.karasek@student.tudelft.nl>
2025-06-06 15:32:58 +0200

    refactoring tests

--- verification/test_power.py
+++ verification/test_power.py
@@ -1,5 +1,8 @@
-from h2ermes_tools.power_mass import power_plant_mass, lb_to_kg
 import pytest
+import numpy as np
+
+from h2ermes_tools.power_mass import power_plant_mass, lb_to_kg
+
 
 def test_power():
     shuttle_power = 14000 # kW
@@ -8,4 +11,4 @@ def test_power():
     # Mass of 2 power cells and contents of 3 oxygen and hydrogen tanks
     ground_truth_mass = 2 * lb_to_kg(255) + 3 * lb_to_kg(781 + 92)
 
-    assert round(calculated_mass, 3) == round(ground_truth_mass, 3)
+    assert np.isclose(calculated_mass, ground_truth_mass, rtol=1e-3)
