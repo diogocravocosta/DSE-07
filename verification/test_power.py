ec122590476674613cafd9dbfc4ff4c27c10e3d7
ghomoki <101978979+ghomoki@users.noreply.github.com>
2025-06-06 12:47:53 +0200

    Fixed test

--- verification/test_power.py
+++ verification/test_power.py
@@ -1,8 +1,11 @@
 from src.h2ermes_tools.power_mass import power_plant_mass, lb_to_kg
+import pytest
 
-shuttle_power = 14000 # kW
+def test_power():
+    shuttle_power = 14000 # kW
+    # Nominal Shuttle consumption is 8 tanks of oxygen and hydrogen over 8 days
+    calculated_mass = power_plant_mass(average_power=shuttle_power, mission_length_days=8, safety_factor=1)
+    # Mass of 2 power cells and contents of 3 oxygen and hydrogen tanks
+    ground_truth_mass = 2 * lb_to_kg(255) + 3 * lb_to_kg(781 + 92)
 
-# Nominal Shuttle consumption is 8 tanks of oxygen and hydrogen over 8 days
-print(power_plant_mass(average_power=shuttle_power, mission_length_days=8, safety_factor=1))
-# Mass of 2 power cells and contents of 3 oxygen and hydrogen tanks
-print(2 * lb_to_kg(255) + 3 * lb_to_kg(781 + 92))
+    assert round(calculated_mass, 3) == round(ground_truth_mass, 3)
