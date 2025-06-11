0115505c63eab307f2dbffcb7e8a422e8ab8b47e
lgarciagutierr-git <lgarciagutierr@tudelft.nl>
2025-06-11 16:53:52 +0200

    start of code for development cost

---
+++ src/h2ermes_tools/development_cost.py
@@ -0,0 +1,7 @@
+
+
+def calc_development_cost():
+    for i in range len(M):
+        component_cost = a * M[i]**b
+        cost_reduction_factor = n ** (ln)
+        FU_component_cost = component_cost*cost_reduction_factor
