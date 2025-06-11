f670e23a3897410d02525ab387627ed4e715377e
lgarciagutierr-git <lgarciagutierr@tudelft.nl>
2025-06-11 16:53:18 +0200

    commit of turbopump mass sizing for integration

--- src/h2ermes_tools/propulsion/cycle_sizing.py
+++ src/h2ermes_tools/propulsion/cycle_sizing.py
@@ -8,6 +8,7 @@ chamber_pressure = 6100000 #in Pa
 #tank_pressure = 270000 #in Pa, lowest pressure at tank
 prop_tank_pressure = 270000
 oxidizer_tank_pressure = 200000
+o = 1
 
 
 propellant_density = 70.85
