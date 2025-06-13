cb97050229ef62e56a1c6233d0f5532cb588e8af
Matyas Karasek <m.karasek@student.tudelft.nl>
2025-06-13 16:06:28 +0200

    finished ellipse plotter

--- src/charts/orbit_diagram.py
+++ src/charts/orbit_diagram.py
@@ -3,15 +3,21 @@ import numpy as np
 
 import data.constants as cn
 
+
 def plot_mission_till_rendezvous(initial_orbit:float,
                                  target_orbit:float,
                                  up_phasing_altitude:float,
                                  down_phasing_altitude:float,
                                  reentry_altitude:float,
-                                 inner_body_radius:float,
+                                 subtract_earth_radius:bool,
                                  plot_inner_body:bool
                                  ):
     fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+
+    if subtract_earth_radius:
+        inner_body_radius = 0
+    else:
+        inner_body_radius = cn.earth_radius
 
     # Set radial axis label
     ax.set_xlabel("Radius [km]")
@@ -36,6 +42,40 @@ def plot_mission_till_rendezvous(initial_orbit:float,
                 label='Target orbit'
                 )
 
+    # plot transfer orbit
+    plot_ellipse(ax=ax,
+                 apoapsis=target_orbit + cn.earth_radius,
+                 periapsis=initial_orbit + cn.earth_radius,
+                 start_theta=np.pi/2,
+                 end_theta=np.pi *3/2,
+                 periapsis_theta=np.pi/2,
+                 label='Transfer orbit',
+                 subtract_earth_radius=subtract_earth_radius
+                 )
+
+
+    # plot down phasing orbit
+    plot_ellipse(ax=ax,
+                 apoapsis=target_orbit + cn.earth_radius,
+                 periapsis=down_phasing_altitude + cn.earth_radius,
+                 start_theta=0,
+                 end_theta=2*np.pi,
+                 periapsis_theta=0,
+                 label='Down phasing orbit',
+                 subtract_earth_radius=subtract_earth_radius
+                 )
+
+    # plot reentry orbit
+    plot_ellipse(ax=ax,
+                 apoapsis=target_orbit + cn.earth_radius,
+                 periapsis=reentry_altitude + cn.earth_radius,
+                 start_theta=np.pi,
+                 end_theta=2*np.pi,
+                 periapsis_theta=0,
+                 label='Reentry orbit',
+                 subtract_earth_radius=subtract_earth_radius
+                 )
+
     ax.set_rmax((up_phasing_altitude + inner_body_radius)/1000 * 1.1)
     ax.grid(True)
     ax.legend()
@@ -43,12 +83,15 @@ def plot_mission_till_rendezvous(initial_orbit:float,
 
 def plot_circle(ax:plt.Axes,
                 radius:float,
+                start_theta:float=0,
+                end_theta:float=2*np.pi,
                 color:str|None = None,
-                label:str|None = None
+                label:str|None = None,
                 ):
-    steps = 100
-    thetas = np.linspace(0, 2*np.pi, steps)
-    radia = np.ones(steps) * radius
+    step = np.pi/100
+
+    thetas = np.arange(start_theta, end_theta + step, step)
+    radia = np.ones_like(thetas) * radius
 
     ax.plot(thetas, radia/1000, color = color, label=label)
 
@@ -56,10 +99,24 @@ def plot_ellipse(ax:plt.Axes,
                  apoapsis:float,
                  periapsis:float,
                  start_theta:float,
-                 end_theta:float
+                 end_theta:float,
+                 periapsis_theta:float,
                  color:str|None = None,
-                 label:str|None = None
+                 label:str|None = None,
+                 subtract_earth_radius:bool = False
                  ):
+    semi_major_axis = (apoapsis + periapsis)/2
+    eccentricity = (apoapsis - periapsis)/(apoapsis+periapsis)
+
+    step = np.pi/100
+
+    thetas = np.arange(start_theta, end_theta + step, step)
+    radia = semi_major_axis * (1-eccentricity**2)/(1+eccentricity*np.cos(thetas - periapsis_theta))
+
+    if subtract_earth_radius:
+        radia -= cn.earth_radius
+
+    ax.plot(thetas, radia/1000, color = color, label=label)
 
 def plot_ascent(ax:plt.Axes,
                 start_theta:float = 0,
@@ -79,7 +136,7 @@ if __name__ == '__main__':
                                  up_phasing_altitude=7e5,
                                  down_phasing_altitude=2e5,
                                  reentry_altitude=5e4,
-                                 inner_body_radius=cn.earth_radius,
+                                 subtract_earth_radius=False,
                                  plot_inner_body=True
                                  )
 
@@ -88,7 +145,7 @@ if __name__ == '__main__':
                                  up_phasing_altitude=7e5,
                                  down_phasing_altitude=2e5,
                                  reentry_altitude=5e4,
-                                 inner_body_radius=cn.earth_radius,
+                                 subtract_earth_radius=False,
                                  plot_inner_body=False
                                  )
 
@@ -97,6 +154,6 @@ if __name__ == '__main__':
                                  up_phasing_altitude=7e5,
                                  down_phasing_altitude=2e5,
                                  reentry_altitude=5e4,
-                                 inner_body_radius=0,
+                                 subtract_earth_radius=True,
                                  plot_inner_body=False
                                  )
