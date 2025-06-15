3a3ea2c76a8fa707f1e267327e5691d5687494d6
Jiří Kovář <j.kovar@student.tudelft.nl>
2025-06-15 21:57:54 +0200

    format variables and add more margins

--- src/h2ermes_tools/variables.py
+++ src/h2ermes_tools/variables.py
@@ -50,7 +50,7 @@ coolant_mass = Variable(
     latex_symbol="M_c",
     confidence="poor",
     constraints="must be positive",
-    margin=(1.0, 1.25),
+    margin=(0.75, 1.25),
 )
 
 heat_shield_thickness = Variable(
@@ -75,7 +75,7 @@ heat_shield_mass = Variable(
 
 orbit_insertion_delta_v = Variable(
     name="Orbit Insertion Delta V",
-    value=5800.,
+    value=5800.0,
     unit="m/s",
     latex_symbol="\\Delta V_{oi}",
     confidence="fair",
@@ -85,7 +85,7 @@ orbit_insertion_delta_v = Variable(
 
 orbit_raising_delta_v = Variable(
     name="Orbit Raising Delta V",
-    value=114.,
+    value=114.0,
     unit="m/s",
     latex_symbol="\\Delta V_{or}",
     confidence="good",
@@ -95,7 +95,7 @@ orbit_raising_delta_v = Variable(
 
 target_orbit_circularization_delta_v = Variable(
     name="Target Orbit Circularization Delta V",
-    value=113.,
+    value=113.0,
     unit="m/s",
     latex_symbol="\\Delta V_{toc}",
     confidence="good",
@@ -105,7 +105,7 @@ target_orbit_circularization_delta_v = Variable(
 
 deorbit_delta_v = Variable(
     name="Deorbit Delta V",
-    value=160.,
+    value=160.0,
     unit="m/s",
     latex_symbol="\\Delta V_{do}",
     confidence="good",
@@ -115,7 +115,7 @@ deorbit_delta_v = Variable(
 
 landing_delta_v = Variable(
     name="Landing Delta V",
-    value=500.,
+    value=500.0,
     unit="m/s",
     latex_symbol="\\Delta V_{l}",
     confidence="poor",
@@ -125,7 +125,7 @@ landing_delta_v = Variable(
 
 drag_compensation_delta_v = Variable(
     name="Drag Compensation Delta V",
-    value=30.,
+    value=30.0,
     unit="m/s",
     latex_symbol="\\Delta V_{dc}",
     confidence="poor",
@@ -135,277 +135,275 @@ drag_compensation_delta_v = Variable(
 
 
 length_combustion_chamber_sl = Variable(
-        name="Length of the Combustion Chamber for Seal Level Optimized Combustion Chambers",
-        value=0.33444,
-        unit="m",
-        latex_symbol="L_{cc_{sl}}",
-        confidence="good, design can change",
-        constraints="only positive values",
-        margin=(1.0, 1.0),
-    )
+    name="Length of the Combustion Chamber for Seal Level Optimized Combustion Chambers",
+    value=0.33444,
+    unit="m",
+    latex_symbol="L_{cc_{sl}}",
+    confidence="good, design can change",
+    constraints="only positive values",
+    margin=(1.0, 1.0),
+)
 
 length_combustion_chamber_vac = Variable(
-        name="Length of the Combustion Chamber for Vacuum Optimized Combustion Chambers",
-        value=0.33184,
-        unit="m",
-        latex_symbol="L_{cc_{vac}}",
-        confidence="good, design can change",
-        constraints="only positive values",
-        margin=(1.0, 1.0),
-    )
+    name="Length of the Combustion Chamber for Vacuum Optimized Combustion Chambers",
+    value=0.33184,
+    unit="m",
+    latex_symbol="L_{cc_{vac}}",
+    confidence="good, design can change",
+    constraints="only positive values",
+    margin=(1.0, 1.0),
+)
 
 length_nozzle_sl = Variable(
-        name="Length of the nozzle for Seal Level Optimized Combustion Chambers",
-        value=0.23551,
-        unit="m",
-        latex_symbol="L_{n_{sl}}",
-        confidence="good, design can change",
-        constraints="only positive values",
-        margin=(1.0, 1.0),
-    )
+    name="Length of the nozzle for Seal Level Optimized Combustion Chambers",
+    value=0.23551,
+    unit="m",
+    latex_symbol="L_{n_{sl}}",
+    confidence="good, design can change",
+    constraints="only positive values",
+    margin=(1.0, 1.0),
+)
 
 length_nozzle_vac = Variable(
-        name="Length of the nozzle for Vacuum Optimized Combustion Chambers",
-        value=0.95595,
-        unit="m",
-        latex_symbol="L_{n_{vac}}",
-        confidence="good, design can change",
-        constraints="only positive values",
-        margin=(1.0, 1.0),
-    )
-
-
+    name="Length of the nozzle for Vacuum Optimized Combustion Chambers",
+    value=0.95595,
+    unit="m",
+    latex_symbol="L_{n_{vac}}",
+    confidence="good, design can change",
+    constraints="only positive values",
+    margin=(1.0, 1.0),
+)
 
 
 diameter_exit_sl = Variable(
-        name="Diameter of the Exit for Sea Level Optimized Combustion Chambers",
-        value=0.25833,
-        unit="m",
-        latex_symbol="D_{e_{sl}}",
-        confidence="good, design can change",
-        constraints="only positive values",
-        margin=(1.0, 1.0),
-    )
+    name="Diameter of the Exit for Sea Level Optimized Combustion Chambers",
+    value=0.25833,
+    unit="m",
+    latex_symbol="D_{e_{sl}}",
+    confidence="good, design can change",
+    constraints="only positive values",
+    margin=(1.0, 1.0),
+)
 
 diameter_exit_vac = Variable(
-        name="Diameter of the Exit for Vacuum Optimized Combustion Chambers",
-        value=0.76550,
-        unit="m",
-        latex_symbol="D_{e_{vac}}",
-        confidence="good, design can change",
-        constraints="only positive values",
-        margin=(1.0, 1.0),
-    )
+    name="Diameter of the Exit for Vacuum Optimized Combustion Chambers",
+    value=0.76550,
+    unit="m",
+    latex_symbol="D_{e_{vac}}",
+    confidence="good, design can change",
+    constraints="only positive values",
+    margin=(1.0, 1.0),
+)
 
 engine_mixture_ratio = Variable(
-        name="The ratio of mass of oxidizer to mass of fuel in the engine",
-        value=6.0,
-        unit="[-]",
-        latex_symbol="O/F",
-        confidence="design parameter",
-        constraints="only positive values",
-        margin=(1.0, 1.0),
-    )
+    name="The ratio of mass of oxidizer to mass of fuel in the engine",
+    value=6.0,
+    unit="[-]",
+    latex_symbol="O/F",
+    confidence="design parameter",
+    constraints="only positive values",
+    margin=(0.9, 1.1),
+)
 
 engine_specific_impulse_sl_opt = Variable(
-        name="The specific impulse of the sea level nozzles in sea level (optimal) conditions",
-        value=361.6451,
-        unit="s",
-        latex_symbol="I_{sp_{sl_{OPT}}}",
-        confidence="Result of the Desgin, can change",
-        constraints="only positive values",
-        margin=(0.9, 1.1),
-    )
+    name="The specific impulse of the sea level nozzles in sea level (optimal) conditions",
+    value=361.6451,
+    unit="s",
+    latex_symbol="I_{sp_{sl_{OPT}}}",
+    confidence="Result of the Desgin, can change",
+    constraints="only positive values",
+    margin=(0.9, 1.1),
+)
 
 engine_specific_impulse_sl_non_opt = Variable(
-        name="The specific impulse of the ses level nozzles in vacuum (non-optimal) conditions",
-        value=393.3471,
-        unit="s",
-        latex_symbol="I_{sp_{sl_{NON-OPT}}}",
-        confidence="Result of the Desgin, can change",
-        constraints="only positive values",
-        margin=(1.0, 1.0),
-    )
+    name="The specific impulse of the ses level nozzles in vacuum (non-optimal) conditions",
+    value=393.3471,
+    unit="s",
+    latex_symbol="I_{sp_{sl_{NON-OPT}}}",
+    confidence="Result of the Desgin, can change",
+    constraints="only positive values",
+    margin=(1.0, 1.0),
+)
 
 engine_specific_impulse_vac_opt = Variable(
-        name="The specific impulse of the vacuum nozzles in vacuum (optimal) conditions",
-        value=447.9481,
-        unit="s",
-        latex_symbol="I_{sp_{sl_{OPT}}}",
-        confidence="Result of the Desgin, can change",
-        constraints="only positive values",
-        margin=(0.95, 1.05),
-    )
+    name="The specific impulse of the vacuum nozzles in vacuum (optimal) conditions",
+    value=447.9481,
+    unit="s",
+    latex_symbol="I_{sp_{sl_{OPT}}}",
+    confidence="Result of the Desgin, can change",
+    constraints="only positive values",
+    margin=(0.95, 1.05),
+)
 
 engine_specific_impulse_vac_non_opt = Variable(
-        name="The specific impulse of the vacuum nozzles in sea level (non-optimal) conditions",
-        value=130.9458,
-        unit="s",
-        latex_symbol="I_{sp_{sl_{NON-OPT}}}",
-        confidence="Result of the Desgin, can change",
-        constraints="only positive values",
-        margin=(1.0, 1.0),
-    )
+    name="The specific impulse of the vacuum nozzles in sea level (non-optimal) conditions",
+    value=130.9458,
+    unit="s",
+    latex_symbol="I_{sp_{sl_{NON-OPT}}}",
+    confidence="Result of the Desgin, can change",
+    constraints="only positive values",
+    margin=(1.0, 1.0),
+)
 
 engine_thrust_sl_opt = Variable(
-        name="The thrust of each of the sea level nozzles in sea level (optimal) conditions",
-        value=61364.9,
-        unit="N",
-        latex_symbol="T_{sl_{OPT}}",
-        confidence="Design Parameter, can change",
-        constraints="only positive values",
-        margin=(1.0, 1.0),
-    )
+    name="The thrust of each of the sea level nozzles in sea level (optimal) conditions",
+    value=61364.9,
+    unit="N",
+    latex_symbol="T_{sl_{OPT}}",
+    confidence="Design Parameter, can change",
+    constraints="only positive values",
+    margin=(0.9, 1.1),
+)
 
 engine_thrust_sl_non_opt = Variable(
-        name="The thrust of each of the sea level nozzles in vacuum (non-optimal) conditions",
-        value=66744.2,
-        unit="N",
-        latex_symbol="T_{sl_{NON-OPT}}",
-        confidence="Result of the Desgin, can change",
-        constraints="only positive values",
-        margin=(1.0, 1.0),
-    )
+    name="The thrust of each of the sea level nozzles in vacuum (non-optimal) conditions",
+    value=66744.2,
+    unit="N",
+    latex_symbol="T_{sl_{NON-OPT}}",
+    confidence="Result of the Desgin, can change",
+    constraints="only positive values",
+    margin=(1.0, 1.0),
+)
 
 engine_thrust_vac_opt = Variable(
-        name="The thrust of each of the vacuum nozzles in vacuum (optimal) conditions",
-        value=66744.2,
-        unit="N",
-        latex_symbol="T_{vac_{OPT}}",
-        confidence="Design Parameter, can change",
-        constraints="only positive values",
-        margin=(1.0, 1.0),
-    )
+    name="The thrust of each of the vacuum nozzles in vacuum (optimal) conditions",
+    value=66744.2,
+    unit="N",
+    latex_symbol="T_{vac_{OPT}}",
+    confidence="Design Parameter, can change",
+    constraints="only positive values",
+    margin=(0.9, 1.1),
+)
 
 engine_thrust_vac_non_opt = Variable(
-        name="The the thrust of each of the vacuum nozzles in sea level (non-optimal) conditions",
-        value=19510.9,
-        unit="N",
-        latex_symbol="T_{vac_{NON-OPT}}",
-        confidence="Result of the Desgin, can change",
-        constraints="only positive values",
-        margin=(1.0, 1.0),
-    )
+    name="The the thrust of each of the vacuum nozzles in sea level (non-optimal) conditions",
+    value=19510.9,
+    unit="N",
+    latex_symbol="T_{vac_{NON-OPT}}",
+    confidence="Result of the Desgin, can change",
+    constraints="only positive values",
+    margin=(1.0, 1.0),
+)
 
 expansion_ratio_vac = Variable(
-        name="The expansion ratio of each of the vacuum nozzles (ratio of the area of the exit and the area of the throat)",
-        value=80,
-        unit="[-]",
-        latex_symbol="A_e/A_t_{vac}",
-        confidence="Design Parameter, can change",
-        constraints="only positive values",
-        margin=(1.0, 1.0),
-    )
+    name="The expansion ratio of each of the vacuum nozzles (ratio of the area of the exit and the area of the throat)",
+    value=80,
+    unit="[-]",
+    latex_symbol="A_e/A_t_{vac}",
+    confidence="Design Parameter, can change",
+    constraints="only positive values",
+    margin=(0.75, 1.25),
+)
 
 expansion_ratio_sl = Variable(
-        name="The expansion ratio of each of the sea level nozzles (ratio of the area of the exit and the area of the throat)",
-        value=8,
-        unit="[-]",
-        latex_symbol="A_e/A_t_{sl}",
-        confidence="Design Parameter, can change",
-        constraints="only positive values",
-        margin=(0.75, 1.25),
-    )
+    name="The expansion ratio of each of the sea level nozzles (ratio of the area of the exit and the area of the throat)",
+    value=8,
+    unit="[-]",
+    latex_symbol="A_e/A_t_{sl}",
+    confidence="Design Parameter, can change",
+    constraints="only positive values",
+    margin=(0.75, 1.25),
+)
 
 n_chambers_sl = Variable(
-        name="The number of sea level thrust chambers used in the design",
-        value=8,
-        unit="[-]",
-        latex_symbol="N_{sl}",
-        confidence="Design Parameter, can change",
-        constraints="only positive values",
-        margin=(1.0, 1.0),
-    )
+    name="The number of sea level thrust chambers used in the design",
+    value=8,
+    unit="[-]",
+    latex_symbol="N_{sl}",
+    confidence="Design Parameter, can change",
+    constraints="only positive values",
+    margin=(1.0, 1.0),
+)
 
 n_chambers_vac = Variable(
-        name="The number of vacuum optimized thrust chambers used in the design",
-        value=16,
-        unit="[-]",
-        latex_symbol="N_{vac}",
-        confidence="Design Parameter, can change",
-        constraints="only positive values",
-        margin=(1.0, 1.0),
-    )
+    name="The number of vacuum optimized thrust chambers used in the design",
+    value=16,
+    unit="[-]",
+    latex_symbol="N_{vac}",
+    confidence="Design Parameter, can change",
+    constraints="only positive values",
+    margin=(1.0, 1.0),
+)
 
 total_dry_mass = Variable(
-        name="The total dry mass of the vehicle",
-        value=20642.21346,
-        unit="kg",
-        latex_symbol="m_s",
-        confidence="low",
-        constraints="only positive values",
-        margin=(0.8, 1.2),
-    )
+    name="The total dry mass of the vehicle",
+    value=20642.21346,
+    unit="kg",
+    latex_symbol="m_s",
+    confidence="low",
+    constraints="only positive values",
+    margin=(0.8, 1.2),
+)
 
 gross_mass = Variable(
-        name="The total mass of the vehicle including propellant",
-        value=246_684.0317,
-        unit="kg",
-        latex_symbol="m_g",
-        confidence="low",
-        constraints="only positive values",
-        margin=(0.8, 1.2),
-    )
+    name="The total mass of the vehicle including propellant",
+    value=246_684.0317,
+    unit="kg",
+    latex_symbol="m_g",
+    confidence="low",
+    constraints="only positive values",
+    margin=(0.8, 1.2),
+)
 
 payload_mass = Variable(
-        name="The payload mass delivered to depot",
-        value=10_000,
-        unit="kg",
-        latex_symbol="m_p",
-        confidence="set",
-        constraints="only positive values",
-        margin=(1.0, 1.0),
-    )
+    name="The payload mass delivered to depot",
+    value=10_000,
+    unit="kg",
+    latex_symbol="m_p",
+    confidence="set",
+    constraints="only positive values",
+    margin=(0.9, 1.1),
+)
 
 thrust_chamber_sl_mass = Variable(
-        name="The mass of a sea level optimized Thrust Chamber",
-        value=9.666,
-        unit="kg",
-        latex_symbol="m_{tc_{sl}}",
-        confidence="medium, conservative value",
-        constraints="only positive values",
-        margin=(0.8, 1.2),
-    )
+    name="The mass of a sea level optimized Thrust Chamber",
+    value=9.666,
+    unit="kg",
+    latex_symbol="m_{tc_{sl}}",
+    confidence="medium, conservative value",
+    constraints="only positive values",
+    margin=(0.8, 1.2),
+)
 
 thrust_chamber_vac_mass = Variable(
-        name="The mass of a vacuum optimized Thrust Chamber",
-        value=66.04,
-        unit="kg",
-        latex_symbol="m_{tc_{vac}}",
-        confidence="medium, conservative value",
-        constraints="only positive values",
-        margin=(0.8, 1.2),
-    )
+    name="The mass of a vacuum optimized Thrust Chamber",
+    value=66.04,
+    unit="kg",
+    latex_symbol="m_{tc_{vac}}",
+    confidence="medium, conservative value",
+    constraints="only positive values",
+    margin=(0.8, 1.2),
+)
 
 mass_flow_main = Variable(
-        name="Mass Flow of the Main Engine, including all of the Thrust Chambers",
-        value=514.4880, 
-        unit="kg/s",
-        latex_symbol="\dot{m}_{main}",
-        confidence="result of other parameters, can change",
-        constraints="only positive values",
-        margin=(0.8, 1.2),
-    )
+    name="Mass Flow of the Main Engine, including all of the Thrust Chambers",
+    value=514.4880,
+    unit="kg/s",
+    latex_symbol="\dot{m}_{main}",
+    confidence="result of other parameters, can change",
+    constraints="only positive values",
+    margin=(0.8, 1.2),
+)
 
 t_w_sl = Variable(
-        name="Thrust to Weight Ratio of the Vehicle during the start of the Landing Burn (only SL engines firing)",
-        value=1.6, 
-        unit="[-]",
-        latex_symbol="T/W_{sl}",
-        confidence="Result of Analysis, set value, will not change",
-        constraints="only positive values",
-        margin=(1, 1),
-    )
+    name="Thrust to Weight Ratio of the Vehicle during the start of the Landing Burn (only SL engines firing)",
+    value=1.6,
+    unit="[-]",
+    latex_symbol="T/W_{sl}",
+    confidence="Result of Analysis, set value, will not change",
+    constraints="only positive values",
+    margin=(0.8, 1.2),
+)
 
 t_w_vac = Variable(
-        name="Thrust to Weight Ratio of the Vehicle during the start of the Vacuum Burn (SL and Vac engines firing)",
-        value=1.2, 
-        unit="[-]",
-        latex_symbol="T/W_{vac}",
-        confidence="Result of Analysis, set value, will not change",
-        constraints="only positive values",
-        margin=(1, 1),
-    )
+    name="Thrust to Weight Ratio of the Vehicle during the start of the Vacuum Burn (SL and Vac engines firing)",
+    value=1.2,
+    unit="[-]",
+    latex_symbol="T/W_{vac}",
+    confidence="Result of Analysis, set value, will not change",
+    constraints="only positive values",
+    margin=(0.8, 1.2),
+)
 
 acs_peak_power = Variable(
     name="ACS Peak Power",
@@ -448,12 +446,12 @@ acs_propellant_mass = Variable(
 )
 
 docking_system_mass = Variable(
-    name='Docking System Mass',
+    name="Docking System Mass",
     value=370,
-    unit='kg',
-    latex_symbol='m_{docking system}',
-    confidence='good',
-    constraints='>0',
+    unit="kg",
+    latex_symbol="m_{docking system}",
+    confidence="good",
+    constraints=">0",
     margin=(1.0, 1.1),
 )
 
@@ -504,7 +502,7 @@ power_dry_mass = Variable(
     latex_symbol="m_{power,dry}",
     confidence="",
     constraints="",
-    margin=(1.0, 1.0)
+    margin=(0.8, 1.2),
 )
 
 avionics_mass = Variable(
@@ -514,7 +512,7 @@ avionics_mass = Variable(
     latex_symbol="m_{power,dry}",
     confidence="",
     constraints="",
-    margin=(1.0, 1.0)
+    margin=(0.8, 1.2),
 )
 
 wiring_mass = Variable(
@@ -524,7 +522,7 @@ wiring_mass = Variable(
     latex_symbol="m_{avionics}",
     confidence="",
     constraints="",
-    margin=(1.0, 1.0)
+    margin=(0.8, 1.2),
 )
 
 interstage_mass = Variable(
@@ -534,7 +532,7 @@ interstage_mass = Variable(
     latex_symbol="m_{interstage}",
     confidence="",
     constraints="",
-    margin=(1.0, 1.0)
+    margin=(0.8, 1.2),
 )
 
 
